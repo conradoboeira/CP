b676142bcd28996143283ca6b9b8220d4231fc7d
conradoboeira <conrado.boeira@acad.pucrs.br>
2019-08-19 00:51:29 -0300

    dale2

--- uva/11572_unique_snowflakes/snowflakes.cpp
+++ uva/11572_unique_snowflakes/snowflakes.cpp
@@ -8,18 +8,18 @@ int main(){
     int c, n, maior, curr;
     long atual;
     cin >> c;
+
     while(c--){
         cin >> n;
         map<long,int> used;
         maior = curr = 0;
         for(int i = 1; i <= n; i++){
             cin >> atual;
+            
             if(used[atual]>= (i-curr)) curr = i - used[atual];
             else curr ++;
             used[atual] = i;
-
             if(curr > maior) maior = curr;
-            //cout << "curr " << curr << endl; 
         }
         cout << maior << endl;
     }
